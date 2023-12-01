04c73d01ff8882dc35f0f6db2e77d06df5f7e314
NiraRain <66909207+NiraRain@users.noreply.github.com>
2023-12-01 22:11:11 +0900

    think final

--- Gaining-Territory-main/machine.py
+++ Gaining-Territory-main/machine.py
@@ -34,7 +34,6 @@ class MACHINE():
 
     def calculate_triangle_score(self, move):
         triangle_score = 0
-        # 모든 선과 비교하여 삼각형 형성 가능 여부 확인
         for line in self.drawn_lines:
             for other_line in self.drawn_lines:
                 if line != other_line and move != line and move != other_line:
@@ -44,12 +43,10 @@ class MACHINE():
         return triangle_score
 
     def forms_triangle(self, line1, line2, line3):
-        # 세 선이 삼각형을 형성하는지 확인
         points = set(line1 + line2 + line3)
         return len(points) == 3
 
     def is_valid_triangle(self, line1, line2, line3):
-        # 삼각형 내부에 다른 점이 없는지 확인
         triangle = Polygon([line1[0], line1[1], line2[0], line2[1], line3[0], line3[1]])
         for point in self.whole_points:
             if point not in line1 and point not in line2 and point not in line3:
@@ -58,7 +55,6 @@ class MACHINE():
         return True
     
     def calculate_linearity_score(self, move):
-        # 선이 기존의 선과 같은 일직선상에 있으면 1점 부여
         for line in self.drawn_lines:
             if self.is_colinear(move, line):
                 return 1 
@@ -67,133 +63,61 @@ class MACHINE():
     def calculate_heuristic_move(self, tmp_score):
         user_score, machine_score = tmp_score
         score_difference = machine_score - user_score
-        #total_score = triangle_score + score_difference
         total_score = score_difference
         return total_score
     
     def parallel_minmax(self, drawn_lines, depth, alpha, beta, maximizing_player, tmpscore, move):
         _, best_move_score = self.minmax(drawn_lines + [move], depth - 1, alpha, beta, not maximizing_player, tmpscore)
         return move, best_move_score
-    
-    """def find_best_selection(self):
-        start = time.time()
-        tmp_score = self.score.copy()
-
-        if len(self.whole_points) == 5:
+ 
+    def find_best_selection(self):
+        available_moves = self.get_available_moves(self.drawn_lines)
+        self.play_time = 0
+        if(len(available_moves)) < 9:
             self.depth_n = 6
-        elif len(self.whole_points) == 10:
+        elif(len(available_moves)) < 13:
+            self.depth_n = 5
+        elif(len(available_moves)) < 20:
             self.depth_n = 4
-        elif len(self.whole_points) == 15:
+        elif(len(available_moves)) < 28:
             self.depth_n = 3
         else:
             self.depth_n = 2
 
-        unconnected_points = [p for p in self.whole_points if p not in set(sum(self.drawn_lines, []))]
-
-        possible_lines = [[p1, p2] for p1 in unconnected_points for p2 in unconnected_points if p1 != p2]
-        valid_lines = [line for line in possible_lines if self.check_availability(line)]
-
-        if valid_lines:
-            for line1 in self.drawn_lines:
-                for line2 in self.drawn_lines:
-                    if line1 != line2:
-                        common_point = set(line1).intersection(set(line2))
-                        if common_point:
-                            common_point = common_point.pop()
-                            for point1 in line1:
-                                if point1 != common_point:
-                                    for point2 in line2:
-                                        if point2 != common_point:
-                                            new_line = [point1, point2]
-                                            if self.check_availability(new_line):
-                                                return new_line
-            return random.choice(valid_lines) 
-        else:
-            available_moves = self.get_available_moves(self.drawn_lines)
-            with ProcessPoolExecutor() as executor:
-                futures = [executor.submit(self.parallel_minmax, self.drawn_lines[:], self.depth_n, float('-inf'), float('inf'), True, tmp_score, move) for move in available_moves]
-
-                results = [future.result() for future in as_completed(futures)]
-
-            best_move = max(results, key=lambda x: x[1])[0]
-            end = time.time()
-            print(f"{end - start:.5f} sec")
-            return best_move"""
- 
-    ####
-    # find_best_selection 점2개 이상 남기 전에, 삼각형을 만들 수 있는 상황이 있다면 
-    # 그 예외상황에서 삼각형을 우선으로 처리하도록 예외처리 
-    def find_best_selection(self):
-        self.play_time = 0
-        if len(self.whole_points) == 5:
-            self.depth_n = 9
-        elif len(self.whole_points) == 10:
-            self.depth_n = 4
-        elif len(self.whole_points) == 15:
-            if self.minmax_count > 5:
-                self.depth_n = 4
-            else:
-                self.depth_n = 3
-        else:
-            if self.minmax_count > 5:
-                self.depth_n = 3
-            else:
-                self.depth_n = 2
-
-        start = time.time()
+        #start = time.time()
         tmp_score = self.score.copy()
 
         unconnected_points = [p for p in self.whole_points if p not in set(sum(self.drawn_lines, []))]
-
-        available_moves = self.get_available_moves(self.drawn_lines)
         
         if len(unconnected_points) >= 2:
             possible_lines = [[p1, p2] for p1 in unconnected_points for p2 in unconnected_points if p1 != p2]
             condition_list1 = [False, True, True, True]
             valid_lines = [line for line in possible_lines if self.check_availability(line, condition_list1)]
 
-            for line1 in self.drawn_lines:
-                for line2 in self.drawn_lines:
-                    if line1 != line2:
-                        common_point = set(line1).intersection(set(line2))
-                        if common_point:
-                            common_point = common_point.pop()
-                            for point1 in line1:
-                                if point1 != common_point:
-                                    for point2 in line2:
-                                        if point2 != common_point:
-                                            new_line = [point1, point2]
-                                            condition_list2 = [False, True, True, False]
-                                            if self.check_availability(new_line, condition_list2):
-                                                return new_line
-
             if valid_lines:
                 return random.choice(valid_lines) 
             else:
                 _, best_line = self.minmax(self.drawn_lines[:], can_move=available_moves, depth=self.depth_n, alpha=float('-inf'), beta=float('inf'), maximizing_player=True, tmpscore=tmp_score)
-                end = time.time()
-                print(f"{end - start:.5f} sec")
+                #end = time.time()
+                #print(f"{end - start:.5f} sec")
                 self.minmax_count += 1
-                print(f"play_time = {self.play_time}, available = {available_moves}")
+                #print(f"play_time = {self.play_time}, available = {len(available_moves)}")
+                #print(self.minmax_count)
                 return best_line
         else:
             _, best_line = self.minmax(self.drawn_lines[:], can_move=available_moves, depth=self.depth_n, alpha=float('-inf'), beta=float('inf'), maximizing_player=True, tmpscore=tmp_score)
             end = time.time()
-            print(f"{end - start:.5f} sec")
+            #print(f"{end - start:.5f} sec")
             self.minmax_count += 1
-            print(f"play_time = {self.play_time}, available = {len(available_moves)}")
+            #print(f"play_time = {self.play_time}, available = {len(available_moves)}")
+            #print(self.minmax_count)
             return best_line
-    
-    # 말단노드까지 score 점수 갱신과 말단노드 도착 후 점수 초기화 작업?
 
     def minmax(self, drawn_lines, can_move, depth, alpha, beta, maximizing_player, tmpscore):
         #print(drawn_lines)
         self.play_time += 1
         if depth == 0 or not can_move:
             score = self.calculate_heuristic_move(tmpscore)
-            #user_score, machine_score = self.check_triangle_score(drawn_lines, tmpscore, maximizing_player)
-            #score = machine_score - user_score
-            #print(f"depth 0 score = {score}")
             return score, None
 
         if maximizing_player:
@@ -206,8 +130,6 @@ class MACHINE():
                 drawn_lines.append(move)
                 new_tmp_score = self.check_triangle_score(drawn_lines, tmpscore, maximizing_player)
                 new_available_moves = self.remove_available_moves(can_move)
-                #print()
-
 
                 eval, _ = self.minmax(drawn_lines, new_available_moves, depth - 1, alpha, beta, False, new_tmp_score)
                 #print(f" move = {move} score = {new_tmp_score}, {eval}, drawn_lines = {drawn_lines}")
@@ -270,12 +192,10 @@ class MACHINE():
     def check_availability(self, line, conditionlist):
         line_string = LineString(line)
 
-        # Must be one of the whole points
         condition1 = True
         if conditionlist[0]:
             condition1 = (line[0] in self.whole_points) and (line[1] in self.whole_points)
         
-        # Must not skip a dot
         condition2 = True
         if conditionlist[1]:
             for point in self.whole_points:
@@ -285,7 +205,6 @@ class MACHINE():
                     if bool(line_string.intersection(Point(point))):
                         condition2 = False
 
-        # Must not cross another line
         condition3 = True
         if conditionlist[2]:
             for l in self.drawn_lines:
@@ -294,7 +213,6 @@ class MACHINE():
                 elif bool(line_string.intersection(LineString(l))):
                     condition3 = False
 
-        # Must be a new line
         condition4 = True
         if conditionlist[3]:
             condition4 = (line not in self.drawn_lines)
